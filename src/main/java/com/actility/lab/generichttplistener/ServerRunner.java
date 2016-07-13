8a30b22f351ff3177a4215d8797e6e00421b43b7
Alexandre Estela <alexandre.estela@actility.com>
2016-07-13 11:13:50 +0200

    Adding support for HTTPS

--- src/main/java/com/actility/lab/generichttplistener/ServerRunner.java
+++ src/main/java/com/actility/lab/generichttplistener/ServerRunner.java
@@ -1,10 +1,34 @@
 package com.actility.lab.generichttplistener;
 
+import org.apache.catalina.connector.Connector;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerFactory;
+import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainerFactory;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ServerRunner {
+
+	@Bean
+	public Integer port() {
+		// HTTP PORT
+		// TODO put it in the properties file next to HTTPS settings
+		return 9080;
+	}
+
+	@Bean
+	public EmbeddedServletContainerFactory servletContainer() {
+		TomcatEmbeddedServletContainerFactory tomcat = new TomcatEmbeddedServletContainerFactory();
+		tomcat.addAdditionalTomcatConnectors(createStandardConnector());
+		return tomcat;
+	}
+
+	private Connector createStandardConnector() {
+		Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
+		connector.setPort(port());
+		return connector;
+	}
 	
 	public static void main(String[] args) throws Exception {
 		SpringApplication.run(ServerRunner.class, args);		
